df60d1f5b84c0cf003f44b31fea4c887301602f5
ivanfsilva <ivanfs27@gmail.com>
2020-02-01 09:53:30 -0300

    Validando atributos desconhecidos

--- src/main/java/br/com/ivanfsilva/mymoneyapi/resource/CategoriaResource.java
+++ src/main/java/br/com/ivanfsilva/mymoneyapi/resource/CategoriaResource.java
@@ -3,7 +3,6 @@ package br.com.ivanfsilva.mymoneyapi.resource;
 import br.com.ivanfsilva.mymoneyapi.model.Categoria;
 import br.com.ivanfsilva.mymoneyapi.repository.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
